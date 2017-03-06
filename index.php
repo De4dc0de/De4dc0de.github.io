7b1f6c648c8b834a4fe5f9ca20c4a15ef9077eca
Antr4x <daniel-long@gmx.de>
2017-03-06 20:57:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Deadc0de - Mainpage</title>
+	<meta charset="utf-8">
+	<link media="screen and (max-device-width: 481px)" rel="stylesheet" type="text/css" href="css/phone.css">
+	<link media="screen and (min-device-width: 481px)" rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header class="fixed">
+		<h1>DEAD<span style="color: limegreen;">C0DE</span></h1>
+		<ul>
+			<li><a href="#" class="active">Main</a></li>
+			<li><a href="toolbase.php">Toolbase</a></li>
+			<li><a href="exploits.php">Exploits</a></li>
+			<li><a href="join_us.php">Join Us</a></li>
+			<li><a href="about_us.php">About Us</a></li>
+			<li class="login_register"><a href="register.php">Register</a></li>
+			<li class="login_register"><a href="login.php">Login</a></li>
+		</ul>
+	</header>
+	<section class="wrap-content">
+		<br/>
+		<h1 style="margin-left: 60px;">WELCOME TO DEAD<span style="color: limegreen;">C0DE</span></h1>
+	</section>
+	<footer class="fixed">
+		<div>
+			<h1 style="margin-bottom: -25px; padding-top: 10px;">DEAD<span style="color: limegreen;">C0DE</span></h1>
+			<h1><span style="font-size: 10px; color: red; display:inline;">No patch for human stupidity</span></h1>
+		</div>
+		<div style="float: right;">
+			<h1 id="join_us"><a href="join_us.php">JOIN<span style="color: limegreen;">US</span></a></h1>
+		</div>
+	</footer>
+</body>
+</html>
